496d2c8cbbb4da395975e448867d0101c14a462b
Andrew Stirn <andrew.stirn@gmail.com>
2020-09-08 13:41:23 -0400

    separate toy experiments script.

---
+++ run_regression_toy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# set mode
+MODE="resume"
+
+# run toy data experiments
+python regression_experiments_v2.py --algorithm "Detlefsen" --dataset toy --batch_iterations 6000 --mode $MODE
+python regression_experiments_v2.py --algorithm "Detlefsen (fixed)" --dataset toy --batch_iterations 6000 --mode $MODE
+declare -a Algorithms=("Gamma-Normal" "LogNormal-Normal")
+declare -a PriorTypes=("MLE" "Standard" "VAMP" "VAMP*" "xVAMP" "xVAMP*" "VBEM*")
+for alg in "${Algorithms[@]}"; do
+  for prior in "${PriorTypes[@]}"; do
+    python regression_experiments_v2.py --dataset toy --algorithm $alg --prior_type $prior --mode $MODE \
+      --batch_iterations 6000 --k 20
+  done
+done
+echo "toy done!"
