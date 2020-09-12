387e2234971c4120b17aa813cc61b947281815a2
Andrew Stirn <andrew.stirn@gmail.com>
2020-09-12 10:12:36 -0400

    VBEM added to toy run_regression_toy.sh

--- run_regression_toy.sh
+++ run_regression_toy.sh
@@ -7,7 +7,7 @@ MODE="resume"
 python regression_experiments_v2.py --algorithm "Detlefsen" --dataset toy --batch_iterations 6000 --mode $MODE
 python regression_experiments_v2.py --algorithm "Detlefsen (fixed)" --dataset toy --batch_iterations 6000 --mode $MODE
 declare -a Algorithms=("Gamma-Normal" "LogNormal-Normal")
-declare -a PriorTypes=("MLE" "Standard" "VAMP" "VAMP*" "xVAMP" "xVAMP*" "VBEM*")
+declare -a PriorTypes=("MLE" "Standard" "VAMP" "VAMP*" "xVAMP" "xVAMP*" "VBEM" "VBEM*")
 for alg in "${Algorithms[@]}"; do
   for prior in "${PriorTypes[@]}"; do
     python regression_experiments_v2.py --dataset toy --algorithm $alg --prior_type $prior --mode $MODE \
