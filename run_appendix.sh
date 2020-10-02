6c78a993a930d43d774d8e3549527d8d4ba14017
Andrew Stirn <andrew.stirn@gmail.com>
2020-10-02 15:55:33 -0400

    deleting deprecated script and results

--- run_appendix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# generates figures showing that uniform VAMP prior fails on heteroscedastic variance
-python regression_models.py --prior vamp_uniform
-
-# generate figures for VAE methods that failed
-python generative_failures.py
