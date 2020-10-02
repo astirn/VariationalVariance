7a0097ab12143c305e9a23067518026fdd81f176
Andrew Stirn <andrew.stirn@gmail.com>
2020-10-01 20:29:32 -0400

    updated run_regression_toy.sh

--- run_regression_toy.sh
+++ run_regression_toy.sh
@@ -4,15 +4,15 @@
 MODE="resume"
 
 # run toy data experiments
-python regression_experiments_v2.py --algorithm "Detlefsen" --dataset toy --mode $MODE
-python regression_experiments_v2.py --algorithm "Detlefsen (fixed)" --dataset toy --mode $MODE
-python regression_experiments_v2.py --algorithm "Normal" --dataset toy --mode $MODE
-python regression_experiments_v2.py --algorithm "Student" --dataset toy --mode $MODE
+python regression_experiments.py --algorithm "Detlefsen" --dataset toy --mode $MODE
+python regression_experiments.py --algorithm "Detlefsen (fixed)" --dataset toy --mode $MODE
+python regression_experiments.py --algorithm "Normal" --dataset toy --mode $MODE
+python regression_experiments.py --algorithm "Student" --dataset toy --mode $MODE
 declare -a Algorithms=("Gamma-Normal")
-declare -a PriorTypes=("MLE" "Standard" "VAMP" "VAMP*" "xVAMP" "xVAMP*" "VBEM" "VBEM*")
+declare -a PriorTypes=("VAP" "Standard" "VAMP" "VAMP*" "xVAMP" "xVAMP*" "VBEM" "VBEM*")
 for alg in "${Algorithms[@]}"; do
   for prior in "${PriorTypes[@]}"; do
-    python regression_experiments_v2.py --dataset toy --algorithm $alg --prior_type $prior --mode $MODE --k 20
+    python regression_experiments.py --dataset toy --algorithm $alg --prior_type $prior --mode $MODE --k 20
   done
 done
 
