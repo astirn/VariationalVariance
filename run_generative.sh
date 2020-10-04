892089385b2d3abc850896ed6268237033442f28
Andrew Stirn <andrew.stirn@gmail.com>
2020-10-04 18:25:39 -0400

    added run_generative.sh (VAE bash experiment script).

---
+++ run_generative.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# set mode
+MODE="resume"
+
+# run toy data experiments
+python generative_experiments.py --dataset "fashion_mnist" --mode $MODE
+python generative_experiments.py --dataset "mnist" --mode $MODE
+
+# print all done
+echo "vae experiments done!"
+
+# run analysis scripts
+python generative_analysis.py
